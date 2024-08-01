9f5ffcdb37c2e9831b084bbe733a0969d3b39edd
Niklas20114552 <niklas.deppermann@outlook.com>
2024-08-01 17:51:28 +0200

    Added traveltime for Outpost <-> Seacrestica

--- network.js
+++ network.js
@@ -1,242 +1,242 @@
 const network = {
     lines: [
         {
-            name: 'UST 1',
+            name: "UST 1",
             stations: [
-                'Evergreen',
-                'Outpost',
-                'Seacrestica',
-                'Pagasa City',
-                'Illyria'
+                "Evergreen",
+                "Outpost",
+                "Seacrestica",
+                "Pagasa City",
+                "Illyria"
             ]
         },
         {
-            name: 'UST 2',
+            name: "UST 2",
             stations: [
-                'Illyria',
-                'Pagasa City',
-                'Seacrestica',
-                'Outpost',
-                'Evergreen'
+                "Illyria",
+                "Pagasa City",
+                "Seacrestica",
+                "Outpost",
+                "Evergreen"
             ]
         },
         {
-            name: 'UST 3',
+            name: "UST 3",
             stations: [
-                'Malus',
-                'Outpost',
-                'Eden'
+                "Malus",
+                "Outpost",
+                "Eden"
             ]
         },
         {
-            name: 'UST 4',
+            name: "UST 4",
             stations: [
-                'Eden',
-                'Outpost',
-                'Malus'
+                "Eden",
+                "Outpost",
+                "Malus"
             ]
         },
         {
-            name: 'USTe 1',
+            name: "USTe 1",
             stations: [
-                'Evergreen',
-                'Banal-Witchita',
-                'Ekilorea'
+                "Evergreen",
+                "Banal-Witchita",
+                "Ekilorea"
             ]
         },
         {
-            name: 'USTe 2',
+            name: "USTe 2",
             stations: [
-                'Ekilorea',
-                'Banal-Witchita',
-                'Evergreen'
+                "Ekilorea",
+                "Banal-Witchita",
+                "Evergreen"
             ]
         }
     ],
     stations: [
         {
-            name: 'Evergreen',
+            name: "Evergreen",
             lines: [
                 {
-                    name: 'UST 1',
+                    name: "UST 1",
                     platform: 0
                 },
                 {
-                    name: 'UST 2',
+                    name: "UST 2",
                     platform: 0
                 },
                 {
-                    name: 'USTe 1',
+                    name: "USTe 1",
                     platform: 0
                 },
                 {
-                    name: 'USTe 2',
+                    name: "USTe 2",
                     platform: 0
                 }
             ]
         },
         {
-            name: 'Banal-Witchita',
+            name: "Banal-Witchita",
             lines: [
                 {
-                    name: 'USTe 1',
+                    name: "USTe 1",
                     platform: 0
                 },
                 {
-                    name: 'USTe 2',
+                    name: "USTe 2",
                     platform: 0
                 }
             ]
         },
         {
-            name: 'Ekilorea',
+            name: "Ekilorea",
             lines: [
                 {
-                    name: 'USTe 1',
+                    name: "USTe 1",
                     platform: 0
                 },
                 {
-                    name: 'USTe 2',
+                    name: "USTe 2",
                     platform: 0
                 }
             ]
         },
         {
-            name: 'Outpost',
+            name: "Outpost",
             lines: [
                 {
-                    name: 'UST 1',
+                    name: "UST 1",
                     platform: 3
                 },
                 {
-                    name: 'UST 2',
+                    name: "UST 2",
                     platform: 4
                 },
                 {
-                    name: 'UST 3',
+                    name: "UST 3",
                     platform: 2
                 },
                 {
-                    name: 'UST 4',
+                    name: "UST 4",
                     platform: 1
                 }
             ]
         },
         {
-            name: 'Seacrestica',
+            name: "Seacrestica",
             lines: [
                 {
-                    name: 'UST 1',
+                    name: "UST 1",
                     platform: 1
                 },
                 {
-                    name: 'UST 2',
+                    name: "UST 2",
                     platform: 2
                 }
             ]
         },
         {
-            name: 'Pagasa City',
+            name: "Pagasa City",
             lines: [
                 {
-                    name: 'UST 1',
+                    name: "UST 1",
                     platform: 1
                 },
                 {
-                    name: 'UST 2',
+                    name: "UST 2",
                     platform: 2
                 }
             ]
         },
         {
-            name: 'Illyria',
+            name: "Illyria",
             lines: [
                 {
-                    name: 'UST 1',
+                    name: "UST 1",
                     platform: 0
                 },
                 {
-                    name: 'UST 2',
+                    name: "UST 2",
                     platform: 0
                 }
             ]
         },
         {
-            name: 'Malus',
+            name: "Malus",
             lines: [
                 {
-                    name: 'UST 3',
+                    name: "UST 3",
                     platform: 0
                 },
                 {
-                    name: 'UST 4',
+                    name: "UST 4",
                     platform: 0
                 }
             ]
         },
         {
-            name: 'Eden',
+            name: "Eden",
             lines: [
                 {
-                    name: 'UST 3',
+                    name: "UST 3",
                     platform: 0
                 },
                 {
-                    name: 'UST 4',
+                    name: "UST 4",
                     platform: 0
                 }
             ]
         }
     ],
     doublelines: [
-        ['UST 1', 'UST 2'],
-        ['UST 3', 'UST 4'],
-        ['USTe 1', 'USTe 2']
+        ["UST 1", "UST 2"],
+        ["UST 3", "UST 4"],
+        ["USTe 1", "USTe 2"]
     ],
     traveltimes: [
         {
-            start: 'Evergreen',
-            end: 'Outpost',
+            start: "Evergreen",
+            end: "Outpost",
             time: 42
         },
         {
-            start: 'Outpost',
-            end: 'Seacrestica',
-            time: 24
+            start: "Outpost",
+            end: "Seacrestica",
+            time: 230
         },
         {
-            start: 'Pagasa City',
-            end: 'Seacrestica',
+            start: "Pagasa City",
+            end: "Seacrestica",
             time: 270
         },
         {
-            start: 'Illyria',
-            end: 'Pagasa City',
+            start: "Illyria",
+            end: "Pagasa City",
             time: 32
         },
         {
-            start: 'Malus',
-            end: 'Outpost',
+            start: "Malus",
+            end: "Outpost",
             time: 114
         },
         {
-            start: 'Eden',
-            end: 'Malus',
+            start: "Eden",
+            end: "Malus",
             time: 60
         },
         {
-            start: 'Eden',
-            end: 'Outpost',
+            start: "Eden",
+            end: "Outpost",
             time: 60
         },
         {
-            start: 'Banal-Witchita',
-            end: 'Evergreen',
+            start: "Banal-Witchita",
+            end: "Evergreen",
             time: 60
         },
         {
-            start: 'Banal-Witchita',
-            end: 'Ekilorea',
+            start: "Banal-Witchita",
+            end: "Ekilorea",
             time: 60
         }
     ],
